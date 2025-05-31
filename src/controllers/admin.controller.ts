9779aa99ae72c016334b4d9f6c7611f29a1e3a31
Yusuf Fadilah Rukmana <yusuffadilah58@gmail.com>
2025-05-31 15:24:09 +0700

    feat: add api for edit product

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -1,12 +1,14 @@
 import { NextFunction, Request, Response } from "express";
 import prisma from "../connection/db";
 import { imageKit } from "../utils/imageKit";
-import { readFileSync } from "fs";
+import { readFileSync, rmSync } from "fs";
 import { Prisma } from "@prisma/client";
+import type { FolderObject } from "imagekit/dist/libs/interfaces";
 
 export const createProduct = async (req: Request, res: Response, next: NextFunction) => {
+    const imagesUploaded = (req.files as { [fieldname: string]: Express.Multer.File[] } || {})
+
     try {
-        const imagesUploaded = (req.files as { [fieldname: string]: Express.Multer.File[] } || {})
         const userId = req.user?.id
         const { name,
             description,
@@ -27,7 +29,7 @@ export const createProduct = async (req: Request, res: Response, next: NextFunct
             })
 
             if (!fileUploadImageKit) throw { msg: 'Gagal upload data', status: 400 }
-            await prisma.product.create({
+            const uploadedProduct = await prisma.product.create({
                 data: {
                     name,
                     description,
@@ -39,6 +41,9 @@ export const createProduct = async (req: Request, res: Response, next: NextFunct
                     ownerId: Number(userId)
                 }
             })
+
+            if (!uploadedProduct) throw { msg: 'Gagal membuat produk', status: 400 }
+            rmSync(imagesUploaded.images[0].path)
         }
 
         res.status(200).json({
@@ -48,8 +53,7 @@ export const createProduct = async (req: Request, res: Response, next: NextFunct
         })
 
     } catch (error) {
-        console.log(error, '<<')
-
+        rmSync(imagesUploaded.images[0].path)
         next(error)
     }
 }
@@ -122,6 +126,81 @@ export const updateProductActive = async (req: Request, res: Response, next: Nex
         })
 
     } catch (error) {
+        next(error)
+    }
+}
+
+export const updateProductInformation = async (req: Request, res: Response, next: NextFunction) => {
+    const imagesUploaded = (req.files as { [fieldname: string]: Express.Multer.File[] } || {})
+
+    try {
+        const { name,
+            description,
+            price,
+            stock,
+            weightGram,
+            isActive } = req.body
+
+        const { idProduct } = req.params
+
+        const findProduct = await prisma.product.findFirst({
+            where: {
+                AND: [
+                    { id: Number(idProduct) },
+                    { deletedAt: null }
+                ]
+            }
+        })
+
+        if (!findProduct || findProduct.deletedAt !== null) throw { msg: 'Produk sudah tidak tersedia', status: 404 }
+        if (!name || !description || !price || !stock || !weightGram) throw { msg: 'Harap diisi terlebih dahulu', status: 400 }
+        if (!imagesUploaded.images || imagesUploaded.images.length === 0) throw { msg: 'Tidak ada data yang di upload', status: 404 }
+
+        const fileNameOnDb = findProduct.imageUrl?.split('/').pop()
+        const findFileName = await imageKit.listFiles({
+            searchQuery: `name = "${fileNameOnDb}"`,
+            limit: 1
+        })
+
+        if (findFileName.length === 0) throw { msg: 'Nama File tidak tersedia', status: 404 }
+
+        const file = findFileName[0] as FolderObject
+        const fileBuffer = readFileSync(imagesUploaded.images[0].path)
+        if (!fileBuffer) throw { msg: 'Ada kesalahan saat membaca file', status: 404 }
+
+        const fileUploadImageKit = await imageKit.upload({
+            file: fileBuffer,
+            fileName: imagesUploaded.images[0].filename,
+            folder: "/products/body-sparepart"
+        })
+
+        if (!fileUploadImageKit) throw { msg: 'Gagal upload gambar', status: 400 }
+
+        const uploadedProduct = await prisma.product.update({
+            where: { id: Number(idProduct) },
+            data: {
+                name,
+                description,
+                price: parseFloat(price),
+                isActive: isActive === 'false' ? false : true,
+                stock: Number(stock),
+                weightGram: Number(weightGram),
+                imageUrl: fileUploadImageKit.url
+            }
+        })
+
+        if (!uploadedProduct) throw { msg: 'Ada kesalahan saat mengupdate data', status: 400 }
+        rmSync(imagesUploaded.images[0].path)
+        if ("fileId" in file) await imageKit.deleteFile(file.fileId as string)
+
+        res.status(200).json({
+            error: false,
+            data: {},
+            message: 'Berhasil mengupdate data'
+        })
+
+    } catch (error) {
+        rmSync(imagesUploaded.images[0].path)
         next(error)
     }
 }
