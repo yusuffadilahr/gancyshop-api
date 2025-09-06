a6ade45d93d0a54912b14580e20a55967784ebac
yusuffadilahr <yusuffadilah58@gmail.com>
2025-09-06 10:53:58 +0700

    fix: fixing data cart and create api for get cart user

--- src/routes/cart.route.ts
+++ src/routes/cart.route.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { verifyToken } from "../middlewares/verifyToken";
-import { addToCart } from "../controllers/cart.controller";
+import { addToCart, getShoppingCartUser } from "../controllers/cart.controller";
 import { addToCartValidation } from "../middlewares/validation";
 import { expressValidatorErrorHandling } from "../middlewares/errorValidation";
+import { checkRoleOnlyUser } from "../middlewares/checkUser";
 
 export const cartRoute = Router();
 cartRoute.post(
@@ -11,4 +12,11 @@ cartRoute.post(
   addToCartValidation,
   expressValidatorErrorHandling,
   addToCart
+);
+
+cartRoute.get(
+  "/cart-user",
+  verifyToken,
+  checkRoleOnlyUser,
+  getShoppingCartUser
 );
